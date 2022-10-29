ec017aebea559862a3e3afbf9b9614c30b77e0a3
Jayson Caburatan <montierdejayson@gmail.com>
2022-10-30 03:01:10 +0800

    Added Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaBeer } from 'react-icons/fa';
+// import {FaGlobeAsia} from 'react-icons/fa';
+function Navbar() { 
+    return(
+        <div className='nav'>   
+            <FaBeer />
+        </div>
+    )
+}
+export default Navbar
